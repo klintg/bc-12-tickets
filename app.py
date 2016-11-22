0fb7382daf8b74fa5d96c02a63c01c903f6223f2
klintg <theklint1@gmail.com>
2016-11-22 11:32:39 +0300

    deleting, editing and listing all events

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 import cmd 
+from db import Database
 
 class EventsTick(cmd.Cmd):
 
+    # creating an event
     def do_event_create(self, *event):
 
         name = input("Event name: ").upper()
         venue = input("Event venue: ").upper()
-        start_date = input("Event start date: YYYY, MM, DD(eg 2017, 02, 18): ")
-        end_date = input("Event end date: YYYY, MM, DD(eg 2017, 02, 18): ")
+        start_date = input("Event start date: YYYY, MM, DD(eg 12/01/2015 16:25): ")
+        end_date = input("Event end date: YYYY, MM, DD(eg 12/01/2015 16:25): ")
         
-        print(name, venue, start_date,">", end_date)
+        # print(name, venue, start_date,">", end_date)
+
+        data = Database()
+        data.new_event(name, venue, start_date, end_date)
+
+    
+    # deleting an event
+    def do_event_delete(self, *event):
+        event_id = int(input("Please enter the event ID to: "))
+        data = Database()
+        data.getrid(event_id)
+
+    # listing all events 
+    def do_event_list(self, *events):
+        data = Database()
+        data.read_all_events(events)
+
+    def do_edit(self, *args):
+        event_id = int(input("Please enter the event ID to be edited: "))
+        new_name = input("New Event name: ").upper()
+        new_venue = input("New Event venue: ").upper()
+        new_start_date = input("Event start date: YYYY, MM, DD(eg 12/01/2015 16:25): ")
+        new_end_date = input("Event end date: YYYY, MM, DD(eg 12/01/2015 16:25): ")
+
+        data = Database()
+        data.edit_data(event_id, new_name, new_venue, new_start_date, new_end_date)
+
+
 
 if __name__ == '__main__':
     EventsTick().cmdloop()
--- db.py
+++ db.py
@@ -12,3 +12,23 @@ class Database:
         with self.conn:
              self.cursor.execute("INSERT INTO events(name, venue, start, end) VALUES ('%s', '%s', '%s', '%s')" % (name, venue, datetime.strptime(start, '%d/%m/%Y %H:%M'), datetime.strptime(end, '%d/%m/%Y %H:%M')))
              self.conn.commit()
+
+    def getrid(self, event_id):
+        self.cursor.execute("DELETE FROM events WHERE ID = (%d)" %(event_id))
+        self.conn.commit()
+    
+    def read_all_events(self, *events):
+        self.cursor.execute("SELECT * FROM events")
+        all =  self.cursor.fetchall()
+        for row in all:
+            print('{}, {}, {}, {}, {}'.format(row[0], row[1], row[2], row[3], row[4]))
+
+    def edit_data(self, event_id, name, venue, start, end):
+        # self.cursor.execute("SELECT *FROM events WHERE ID = (%d)" %(event_id))
+        # spec = self.cursor.fetchone()
+        # print(spec)
+
+        # updating the data.
+
+        self.cursor.execute("UPDATE events SET name = '%s', venue = '%s', start ='%s', end = '%s' WHERE ID = %s" % (name, venue, datetime.strptime(start, '%d/%m/%Y %H:%M'), datetime.strptime(end, '%d/%m/%Y %H:%M'), event_id))
+        self.conn.commit()
